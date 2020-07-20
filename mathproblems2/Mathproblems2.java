3c72d5d873293353dfd5a263452ccc2c3c88e6fc
Evangelos Tsivas <62053927+EvangelosTsivas@users.noreply.github.com>
2020-07-20 13:13:16 +0300

    Add files via upload

---
+++ mathproblems2/Mathproblems2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathproblems2;
+
+/**
+ *
+ * @author Vagos
+ */
+public class Mathproblems2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 125;
+        int b = 24;
+        int sum = a+b;
+        int sub = a-b;
+        int multi= a*b;
+        int div = a/b;
+        int mod = a%b;
+        System.out.println(a + " + " + b + " = " +sum);
+        System.out.println(a + " - " + b + " = " +sub);
+        System.out.println(a + " x " + b + " = " +multi);
+        System.out.println(a + " / " + b + " = " +div);
+        System.out.println(a + " mod " + b + " = " +mod);
+    }
+    
+}
