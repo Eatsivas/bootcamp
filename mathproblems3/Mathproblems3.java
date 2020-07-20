5246da61dc0f9e698d5b5f8d72ccf4c3934cee2b
Evangelos Tsivas <62053927+EvangelosTsivas@users.noreply.github.com>
2020-07-20 13:13:28 +0300

    Add files via upload

---
+++ mathproblems3/Mathproblems3.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mathproblems3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Vagos
+ */
+public class Mathproblems3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please select a number");
+        int num = input.nextInt();
+        System.out.println(num + " x 1 = " +(num*1));
+        System.out.println(num + " x 2 = " +(num*2));
+        System.out.println(num + " x 3 = " +(num*3));
+        System.out.println(num + " x 4 = " +(num*4));
+        System.out.println(num + " x 5 = " +(num*5));
+        System.out.println(num + " x 6 = " +(num*6));
+        System.out.println(num + " x 7 = " +(num*7));
+        System.out.println(num + " x 8 = " +(num*8));
+        System.out.println(num + " x 9 = " +(num*9));
+        System.out.println(num + " x 10 = " +(num*10));
+       
+        
+        
+        
+        
+    }
+    
+}
