e7de22bae14a604623f4130bbbf8cb74e8b3fc67
Evangelos Tsivas <62053927+EvangelosTsivas@users.noreply.github.com>
2020-07-20 13:12:38 +0300

    Add files via upload

---
+++ loopstar/LoopStar.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loopstar;
+
+/**
+ *
+ * @author Vagos
+ */
+public class LoopStar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        fourStar();
+
+    }
+
+    static void fourStar() {
+        for (int i = 0; i < 4; i++) {
+            System.out.println("****");
+        }
+    }
+
+}
